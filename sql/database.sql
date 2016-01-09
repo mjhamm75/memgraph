77fc5fa75134b283557ab4a2ec49c1aa677c5911
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-09 13:16:30 -0700

    Add permissions to address table

--- sql/database.sql
+++ sql/database.sql
@@ -9,4 +9,8 @@ GRANT ALL PRIVILEGES ON TABLE member TO memgraph;
 GRANT ALL PRIVILEGES ON SEQUENCE member_id_seq TO memgraph;
 GRANT USAGE, SELECT ON SEQUENCE member_id_seq TO memgraph;
 
-create table address (id serial, streetAddress varchar(100), city varchar(50), state varchar(20), zipcode varchar(10), member_id integer references member(id));	
+create table address (id serial, streetAddress varchar(100), city varchar(50), state varchar(20), zipcode varchar(10), member_id integer references member(id));
+
+GRANT ALL PRIVILEGES ON TABLE address TO memgraph;
+GRANT ALL PRIVILEGES ON SEQUENCE address_id_seq TO memgraph;
+GRANT USAGE, SELECT ON SEQUENCE address_id_seq TO memgraph;
