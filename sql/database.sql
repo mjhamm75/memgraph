ace6896e5aedcedfa737b156206a578c1010bce9
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-08 18:44:19 -0700

    Create new table with address

--- sql/database.sql
+++ sql/database.sql
@@ -8,3 +8,5 @@ create table member (id serial, firstName varchar(50), lastName varchar(75));
 GRANT ALL PRIVILEGES ON TABLE member TO memgraph;
 GRANT ALL PRIVILEGES ON SEQUENCE member_id_seq TO memgraph;
 GRANT USAGE, SELECT ON SEQUENCE member_id_seq TO memgraph;
+
+create table address (id serial, streetAddress varchar(100), city varchar(50), state varchar(20), zipcode varchar(10), member_id integer, foreign key (member_id) references member(id))
