f9782afd5691763d6ba2eacd836211f26453e0a8
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-09 13:14:32 -0700

    Sql to create address table

--- sql/database.sql
+++ sql/database.sql
@@ -3,10 +3,10 @@ create role memgraph;
 alter role memgraph with password 'memgraph';
 alter role memgraph with login;
 
-create table member (id serial, firstName varchar(50), lastName varchar(75));
+create table member (id serial not null primary key, firstName varchar(50), lastName varchar(75));
 
 GRANT ALL PRIVILEGES ON TABLE member TO memgraph;
 GRANT ALL PRIVILEGES ON SEQUENCE member_id_seq TO memgraph;
 GRANT USAGE, SELECT ON SEQUENCE member_id_seq TO memgraph;
 
-create table address (id serial, streetAddress varchar(100), city varchar(50), state varchar(20), zipcode varchar(10), member_id integer);	
+create table address (id serial, streetAddress varchar(100), city varchar(50), state varchar(20), zipcode varchar(10), member_id integer references member(id));	
