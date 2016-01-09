13113288590db57364e54ea4a00e2180a6160366
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-09 13:59:30 -0700

    Update mock to create address

--- mock/fake.js
+++ mock/fake.js
@@ -1,17 +1,40 @@
 import faker from 'faker';
 import { knexInit } from '../config/knex';
+import { Observable } from 'rx';
 
 let knex = knexInit();
 
-for(let index = 0; index < 300; index++) {
+for(let index = 0; index < 400; index++) {
 	var firstname = faker.name.firstName();
 	var lastname = faker.name.lastName();
 	
+	var subscriber = Observable.fromPromise(createName())
+		.concatMap(res => res)
+		.concatMap(res => createAddress(res))
+	subscriber.subscribe(res => console.log(res));
+}
 
-	knex('member').insert({
+function createName() {
+	var firstname = faker.name.firstName();
+	var lastname = faker.name.lastName();
+
+	return knex('member').insert({ 
 		firstname, 
 		lastname
-	}).then(res => {
-		console.log(`Inserted ${res}`)
-	})
+	}).returning('id');
+}
+
+function createAddress(memberId) {
+	var streetaddress = faker.address.streetAddress();
+	var city = faker.address.city();
+	var state = faker.address.state();
+	var zipcode = faker.address.zipCode();
+
+	return knex('address').insert({
+		streetaddress,
+		city,
+		state,
+		zipcode,
+		member_id: memberId
+	}).returning('id');
 }
